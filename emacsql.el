8f3d0d4b81e17a0aac0449ca10fafa679cb6deec
Jonas Bernoulli <jonas@bernoul.li>
2016-08-24 22:07:11 +0200

    Use font-lock-flush and font-lock-ensure if available
    `font-lock-fontity-buffer' is only intended for interactive use.  Emacs
    v25.1 adds `font-lock-flush' and `font-lock-ensure' as replacements.  A
    lot of reading revealed that we actually have to call both functions.

--- emacsql.el
+++ emacsql.el
@@ -340,7 +340,14 @@ Each column must be a plain symbol, no expressions allowed here."
            (sql-mode)
            (with-no-warnings ;; autoloaded by previous line
              (sql-highlight-sqlite-keywords))
-           (font-lock-fontify-buffer)
+           (if (and (fboundp 'font-lock-flush)
+                    (fboundp 'font-lock-ensure))
+               (save-restriction
+                 (widen)
+                 (font-lock-flush)
+                 (font-lock-ensure))
+             (with-no-warnings
+               (font-lock-fontify-buffer)))
            (emacsql--indent)
            (buffer-string))))
     (with-current-buffer (get-buffer-create emacsql-show-buffer-name)
