2ddbf92415c2ff83074adf22b12518379716d87e
Christopher Wellons <wellons@nullprogram.com>
2022-02-18 21:05:22 -0500

    Rewrite most of the backend
    I've learned a lot more about C in the past 8 years, and this is more in
    line with how I'd write it today. This version is more efficient, more
    precise, allocates less, and has much more thorough error checking.

--- sqlite/emacsql.c
+++ sqlite/emacsql.c
@@ -5,179 +5,179 @@
 #include <string.h>
 #include "sqlite3.h"
 
-#define TRUE 1
-#define FALSE 0
+static int
+send_error(int code, const char *message)
+{
+    if (printf("error %d \"", code) < 0) {
+        return 0;
+    }
 
-char* escape(const char *message) {
-    int i, count = 0, length_orig = strlen(message);
-    for (i = 0; i < length_orig; i++) {
-        if (strchr("\"\\", message[i])) {
-            count++;
+    for (;;) {
+        size_t n = strcspn(message, "\"\\");
+        if (n && !fwrite(message, n, 1, stdout)) {
+            return 0;
+        }
+
+        message += n;
+        if (!*message) {
+            return fwrite("\"\n", 2, 1, stdout);
+        }
+
+        char esc[2] = {'\\', *message++};
+        if (!fwrite(esc, 2, 1, stdout)) {
+            return 0;
         }
     }
-    char *copy = malloc(length_orig + count + 1);
-    char *p = copy;
-    while (*message) {
-        if (strchr("\"\\", *message)) {
-            *p = '\\';
-            p++;
-        }
-        *p = *message;
-        message++;
-        p++;
-    }
-    *p = '\0';
-    return copy;
 }
 
-void send_error(int code, const char *message) {
-    char *escaped = escape(message);
-    printf("error %d \"%s\"\n", code, escaped);
-    free(escaped);
-}
+int
+main(int argc, char **argv)
+{
+    char *buf = 0;
+    size_t buflen = 0;
 
-typedef struct {
-    char *buffer;
-    size_t size;
-} buffer;
-
-buffer* buffer_create() {
-    buffer *buffer = malloc(sizeof(*buffer));
-    buffer->size = 4096;
-    buffer->buffer = malloc(buffer->size * sizeof(char));
-    return buffer;
-}
-
-int buffer_grow(buffer *buffer) {
-    unsigned factor = 2;
-    char *newbuffer = realloc(buffer->buffer, buffer->size * factor);
-    if (newbuffer == NULL) {
-        return FALSE;
-    }
-    buffer->buffer = newbuffer;
-    buffer->size *= factor;
-    return TRUE;
-}
-
-int buffer_read(buffer *buffer, size_t count) {
-    while (buffer->size < count + 1) {
-        if (buffer_grow(buffer) == FALSE) {
-            return FALSE;
-        }
-    }
-    size_t in = fread((void *) buffer->buffer, 1, count, stdin);
-    buffer->buffer[count] = '\0';
-    return in == count;
-}
-
-void buffer_free(buffer *buffer) {
-    free(buffer->buffer);
-    free(buffer);
-}
-
-int main(int argc, char **argv) {
-    char *file = NULL;
-    if (argc != 2) {
-        fprintf(stderr,
-                "error: require exactly one argument, the DB filename\n");
-        exit(EXIT_FAILURE);
-    } else {
-        file = argv[1];
-    }
-
-    /* On Windows stderr is not always unbuffered. */
-#if defined(_WIN32) || defined(WIN32) || defined(__MINGW32__)
-    setvbuf(stderr, NULL, _IONBF, 0);
+#ifdef _WIN32
+    int _setmode(int, int);
+    _setmode(0, 0x8000);
+    _setmode(1, 0x8000);
+    setvbuf(stderr, 0, _IONBF, 0);
 #endif
 
-    sqlite3* db = NULL;
-    if (sqlite3_initialize() != SQLITE_OK) {
-        fprintf(stderr, "error: failed to initialize sqlite\n");
-        exit(EXIT_FAILURE);
+    if (argc != 2) {
+        static const char usage[] = "usage: emacsql-sqlite DBFILE\n";
+        fwrite(usage, sizeof(usage)-1, 1, stderr);
+        goto fail;
     }
+    char *dbfile = argv[1];
+
+    sqlite3* db;
     int flags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE;
-    if (sqlite3_open_v2(file, &db, flags, NULL) != SQLITE_OK) {
-        fprintf(stderr, "error: failed to open %s\n", file);
-        exit(EXIT_FAILURE);
+    if (sqlite3_open_v2(dbfile, &db, flags, 0) != SQLITE_OK) {
+        send_error(sqlite3_errcode(db), sqlite3_errmsg(db));
+        goto fail;
     }
 
-    buffer *input = buffer_create();
-    while (TRUE) {
-        printf("#\n");
-        fflush(stdout);
-
-        /* Gather input from Emacs. */
-        unsigned length;
-        int result = scanf("%u ", &length);
-        if (result == EOF) {
-            break;
-        } else if (result != 1) {
-            send_error(SQLITE_ERROR, "middleware parsing error");
-            break;  /* stream out of sync: quit program */
-        }
-        if (!buffer_read(input, length)) {
-            send_error(SQLITE_NOMEM, "middleware out of memory");
-            continue;
+    for (;;) {
+        if (!fwrite("#\n", 2, 1, stdout) || fflush(stdout)) {
+            goto fail;
         }
 
-        /* Parse SQL statement. */
-        sqlite3_stmt *stmt = NULL;
-        result = sqlite3_prepare_v2(db, input->buffer, length, &stmt, NULL);
-        if (result != SQLITE_OK) {
-            send_error(sqlite3_errcode(db), sqlite3_errmsg(db));
-            continue;
-        }
-
-        /* Print out rows. */
-        int first = TRUE, ncolumns = sqlite3_column_count(stmt);
-        printf("(");
-        while (sqlite3_step(stmt) == SQLITE_ROW) {
-            if (first) {
-                printf("(");
-                first = FALSE;
-            } else {
-                printf("\n (");
-            }
-            int i;
-            for (i = 0; i < ncolumns; i++) {
-                if (i > 0) {
-                    printf(" ");
+        // format: BASE-10-LENGTH SP SQL LF
+        size_t sqllen = 0;
+        for (int first = 1;; first = 0) {
+            int c = fgetc(stdin);
+            switch (c) {
+            default:
+                send_error(SQLITE_ERROR, "emacsql parse error");
+                goto fail;
+            case EOF:
+                if (first && !ferror(stdin) && !fflush(stdout)) {
+                    free(buf);
+                    return 0;
                 }
-                int type = sqlite3_column_type(stmt, i);
-                switch (type) {
+                goto fail;
+            case '0': case '1': case '2': case '3': case '4':
+            case '5': case '6': case '7': case '8': case '9':
+                if (sqllen > ((size_t)-1)/20) {
+                    send_error(SQLITE_NOMEM, "buffer too large");
+                    goto fail;
+                }
+                sqllen = sqllen*10 + c - '0';
+                continue;
+            case ' ':
+                break;
+            }
+            break;
+        }
+
+        if (buflen < sqllen+1) {
+            void *p = realloc(buf, sqllen+1);
+            if (!p) {
+                send_error(SQLITE_NOMEM, "out of memory");
+                goto fail;
+            }
+            buf = p;
+            buflen = sqllen+1;
+        }
+
+        if (!fread(buf, sqllen+1, 1, stdin)) {
+            goto fail;
+        }
+
+        sqlite3_stmt *stmt = 0;
+        if (sqlite3_prepare_v2(db, buf, sqllen, &stmt, 0) != SQLITE_OK) {
+            if (!send_error(sqlite3_errcode(db), sqlite3_errmsg(db))) {
+                goto fail;
+            }
+            continue;
+        }
+
+        if (fputc('(', stdout) == EOF) {
+            goto fail;
+        }
+
+        int ncolumns = sqlite3_column_count(stmt);
+        for (int first = 1; sqlite3_step(stmt) == SQLITE_ROW; first = 0) {
+            if (first) {
+                if (fputc('(', stdout) == EOF) {
+                    goto fail;
+                }
+            } else if (!fwrite("\n (", 3, 1, stdout)) {
+                goto fail;
+            }
+
+            for (int i = 0; i < ncolumns; i++) {
+                if (i > 0) {
+                    if (fputc(' ', stdout) == EOF) {
+                        goto fail;
+                    }
+                }
+
+                switch (sqlite3_column_type(stmt, i)) {
                 case SQLITE_INTEGER:
-                    printf("%lld", sqlite3_column_int64(stmt, i));
+                    printf("%lld", (long long)sqlite3_column_int64(stmt, i));
                     break;
                 case SQLITE_FLOAT:
-                    printf("%f", sqlite3_column_double(stmt, i));
+                    printf("%#.17g", sqlite3_column_double(stmt, i));
                     break;
                 case SQLITE_NULL:
-                    printf("nil");
+                    fwrite("nil", 3, 1, stdout);
                     break;
                 case SQLITE_TEXT:
                     fwrite(sqlite3_column_text(stmt, i), 1,
                            sqlite3_column_bytes(stmt, i), stdout);
                     break;
                 case SQLITE_BLOB:
-                    printf("nil");
+                    fwrite("nil", 3, 1, stdout);
                     break;
                 }
+                if (ferror(stdout)) {
+                    goto fail;
+                }
             }
-            printf(")");
+
+            if (fputc(')', stdout) == EOF) {
+                goto fail;
+            }
         }
-        printf(")\n");
+
+        if (fputc(')', stdout) == EOF) {
+            goto fail;
+        }
+
         if (sqlite3_finalize(stmt) != SQLITE_OK) {
-            /* Despite any error code, the statement is still freed.
-             * http://stackoverflow.com/a/8391872
-             */
-            send_error(sqlite3_errcode(db), sqlite3_errmsg(db));
+            if (!send_error(sqlite3_errcode(db), sqlite3_errmsg(db))) {
+                goto fail;
+            }
         } else {
-            printf("success\n");
+            static const char success[] = "success\n";
+            if (!fwrite(success, sizeof(success)-1, 1, stdout)) {
+                goto fail;
+            }
         }
     }
-    buffer_free(input);
 
-    sqlite3_close(db);
-    sqlite3_shutdown();
-    return EXIT_SUCCESS;
+fail:
+    free(buf);
+    return 1;
 }
