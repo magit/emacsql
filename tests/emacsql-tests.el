c56cc2ee51c3144ba4ea1781667a07260a214fd9
Stefan Arentz <stefan@arentz.ca>
2022-10-09 10:26:02 -0400

    GitHub Workflow for testing

--- tests/emacsql-tests.el
+++ tests/emacsql-tests.el
@@ -8,6 +8,8 @@
 (require 'emacsql-compiler-tests)
 (require 'emacsql-external-tests)
 
+(setq truncate-partial-width-windows nil)
+
 ;; Print testing information
 (cl-eval-when (load eval)
   (princ (format "\nTesting %d database(s): %S\n"
