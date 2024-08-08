b9f19ac5e17a90d5b7314d67e3b790992be7d82d
Jonas Bernoulli <jonas@bernoul.li>
2024-08-08 22:16:43 +0200

    Stub libraries for migration

---
+++ emacsql-sqlite-module.el
@@ -0,0 +1,29 @@
+;;; emacsql-sqlite-module.el --- This package has been merged into emacsql  -*- lexical-binding:t -*-
+
+;; This is free and unencumbered software released into the public domain.
+
+;; Author: Jonas Bernoulli <emacs.emacsql@jonas.bernoulli.dev>
+;; Maintainer: Jonas Bernoulli <emacs.emacsql@jonas.bernoulli.dev>
+;; Homepage: https://github.com/magit/emacsql
+;; Package-Version: 4.0.0
+;; SPDX-License-Identifier: Unlicense
+
+;;; Commentary:
+
+;; This package has been merged into `emacsql' itself.
+
+;;; Code:
+
+(display-warning 'emacsql "Uninstall all `emacsql-*' packages.
+
+All EmacSQL back-ends are now distributed as part of the `emacsql'
+package itself, and you must uninstall all `emacsql-*' packages.
+These packages now do nothing but display this warning, but if they
+are located earlier on the `load-path' than `emacsql' is, then they
+prevent the respective libraries from `emacsql' from being loaded,
+rendering EmacSQL unusable.
+" :emergency)
+
+(provide 'emacsql-sqlite-module)
+
+;;; emacsql-sqlite-module.el ends here
