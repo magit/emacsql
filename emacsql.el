9c37ae13c881b18de1ae6d8a5828c62b96c472c6
Jonas Bernoulli <jonas@bernoul.li>
2025-08-29 16:49:32 +0200

    Extend library commentary

--- emacsql.el
+++ emacsql.el
@@ -18,6 +18,11 @@
 ;; PostgreSQL and MySQL are also supported, but use of these connectors
 ;; is not recommended.
 
+;; Any readable lisp value can be stored as a value in EmacSQL,
+;; including numbers, strings, symbols, lists, vectors, and closures.
+;; EmacSQL has no concept of TEXT values; it's all just lisp objects.
+;; The lisp object `nil' corresponds 1:1 with NULL in the database.
+
 ;; See README.md for much more complete documentation.
 
 ;;; Code:
