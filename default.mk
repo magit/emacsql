8ff06f99438986d32cb00b6125258b360ff8f701
Jonas Bernoulli <jonas@bernoul.li>
2026-01-03 23:05:50 +0100

    Rearrange default.mk
    For consistency with other packages I maintain.

--- default.mk
+++ default.mk
@@ -1,5 +1,7 @@
 TOP := $(dir $(lastword $(MAKEFILE_LIST)))
 
+DOMAIN ?= magit.vc
+
 PKG = emacsql
 
 ELS   = $(PKG)-compiler.el
@@ -24,10 +26,6 @@ LOAD_PATH ?= $(addprefix -L ../,$(DEPS))
 LOAD_PATH += -L .
 LOAD_PATH += -L ./test
 
-ifdef NIX_PATH
-export SQLITE3_API_BUILD_COMMAND = nix-shell -p sqlite.dev --run "make all"
-endif
-
 ifeq ($(CI), true)
 # Workaround for bug#58252 on Emacs 28.x.
 override EMACS_ARGS += --eval "(setq byte-compile-docstring-max-column 120)"
@@ -46,6 +44,9 @@ GITSTATS      ?= gitstats
 GITSTATS_DIR  ?= stats
 GITSTATS_ARGS ?= -c style=https://magit.vc/assets/stats.css -c max_authors=999
 
-DOMAIN      ?= magit.vc
 CFRONT_DIST ?= E2LUHBKU1FBV02
 S3_BUCKET   ?= s3://$(DOMAIN)
+
+ifdef NIX_PATH
+export SQLITE3_API_BUILD_COMMAND = nix-shell -p sqlite.dev --run "make all"
+endif
